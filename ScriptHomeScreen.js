b15bda859683b2c7d9e67a2424225c54f228c1d1
megan96 <meghabhatt1996@gmail.com>
2018-01-09 14:27:20 +0530

    Create ScriptHomeScreen.js

---
+++ ScriptHomeScreen.js
@@ -0,0 +1,16 @@
+function button1Click() {
+  document.body.style.backgroundColor = "rgba(102,102,102,0.4)";
+  document.getElementById('id1').style.display='block';
+  }
+  function button1Close(){
+    document.body.style.backgroundColor = "white";
+    document.getElementById('id1').style.display='none';
+  }
+  function button2Click() {
+  document.body.style.backgroundColor = "rgba(102,102,102,0.4)";
+  document.getElementById('id2').style.display='block';
+  }
+  function button2Close(){
+    document.body.style.backgroundColor = "white";
+    document.getElementById('id2').style.display='none';
+  }
